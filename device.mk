2e25c80cb62b213655f9b5448d6b2b851b2537f3
NganuCoeg <starterpackmeh@gmail.com>
2021-09-23 00:26:01 +0800

    r5x: disable some boot_jars

--- device.mk
+++ device.mk
@@ -124,7 +124,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml \
     $(LOCAL_PATH)/permissions/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml
 
-PRODUCT_BOOT_JARS += \
+#PRODUCT_BOOT_JARS += \
     QPerformance \
     UxPerformance
 
